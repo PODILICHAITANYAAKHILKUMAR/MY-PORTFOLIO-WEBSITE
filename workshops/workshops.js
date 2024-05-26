92e7ee20ccac34f9d0a4f5c3d0293b0e77172da1
P CH AKHIL KUMAR <pchakhilkumar1082001@gmail.com>
2024-05-26 16:32:11 +0530

    Add files via upload

---
+++ workshops/workshops.js
@@ -0,0 +1,10 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('img',{interval: 200});
